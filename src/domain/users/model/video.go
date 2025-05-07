1b5fdd0342cdc0252c9900a7db96f66d9152337d
rahnoff <72708227+rahnoff@users.noreply.github.com>
2025-05-07 22:36:36 +0300

    Update video.go

--- src/domain/users/model/video.go
+++ src/domain/users/model/video.go
@@ -4,17 +4,18 @@ import "golang-gin-cassandra/src/utils/errors"
 
 
 type Video struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
-	Genre string `json:"genre"`
-	Language string `json:"language"`
+	Genre       string `json:"genre"`
+	Language    string `json:"language"`
 	StorageLink string `json:"storage_link"`
 }
 
 func (video *Video) ValidateVideo() *errors.RestErr {
-	if video.Title == "" {
-		return errors.NewInternalServerError("Title can't be blank", nil)
+	if (video.Title == "") {
+	    return errors.NewInternalServerError("Title can't be blank", nil)
 	}
+	
 	return nil
 }
